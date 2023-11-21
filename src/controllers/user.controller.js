7842418ad962bb7ecf94bc9eb08c9b3c15e3ddbc
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2023-11-22 01:24:44 +0500

    Route Added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asynHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const registerUser = asyncHandler(async (req, res) => {});
+
+export { registerUser };
