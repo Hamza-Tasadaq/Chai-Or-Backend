d2c88dd10936575d65104758c09846da86df08de
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2023-12-22 12:33:05 +0500

    Loged And Register Implementation

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,10 +5,7 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
+    cb(null, file.originalname);
   },
 });
 
